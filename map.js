73c01bc989b03b396b8d79925d5fb00753a97958
imsanthosh7 <154437536+imsanthosh7@users.noreply.github.com>
2025-05-01 17:56:37 +0530

    update

--- map.js
+++ map.js
@@ -5,26 +5,29 @@ import { farmers } from './farmers.js';
 //  Initialize map
 const map = L.map('map').setView([13.1088, 77.6521], 12);
 
+map.removeControl(map.zoomControl);
+
+
 L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://carto.com/">CARTO</a>',
-    subdomains: 'abcd',
-    maxZoom: 16
+  attribution: '&copy; <a href="https://carto.com/">CARTO</a>',
+  subdomains: 'abcd',
+  maxZoom: 16
 }).addTo(map);
 
 
 
 // Add farmer markers
 farmers.forEach(farmer => {
-    const blinkingIcon = L.divIcon({
-        className: '',
-        html: `<div class="blinking-dot"></div>`,
-        iconSize: [16, 16],
-        iconAnchor: [8, 8]
-    });
+  const blinkingIcon = L.divIcon({
+    className: '',
+    html: `<div class="blinking-dot"></div>`,
+    iconSize: [16, 16],
+    iconAnchor: [8, 8]
+  });
 
-    const marker = L.marker([farmer.lat, farmer.lng], { icon: blinkingIcon }).addTo(map);
+  const marker = L.marker([farmer.lat, farmer.lng], { icon: blinkingIcon }).addTo(map);
 
-    marker.bindTooltip(`
+  marker.bindTooltip(`
     <div class="custom-popup">
       <img src="${farmer.image}" alt="${farmer.name}">
       <div class="popup-text">
@@ -33,10 +36,10 @@ farmers.forEach(farmer => {
       </div>
     </div>
   `, {
-        direction: 'top',
-        offset: [0, -10],
-        sticky: true,
-        opacity: 1,
-        className: ''
-    });
+    direction: 'top',
+    offset: [0, -10],
+    sticky: true,
+    opacity: 1,
+    className: ''
+  });
 });
--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 // navbar section 
 const menuBtn = document.getElementById("nav-btn");
 const navLinks = document.getElementById("nav-menu");
@@ -47,3 +42,20 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+// Go to Top button
+const goToTopButton = document.getElementById('go-to-top');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    goToTopButton.style.display = 'flex';
+  } else {
+    goToTopButton.style.display = 'none';
+  }
+});
+
+goToTopButton.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
