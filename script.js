291bdf5ccdd5c0feb3b812556baaaef76c6bc523
imsanthosh7 <154437536+imsanthosh7@users.noreply.github.com>
2025-05-19 10:58:08 +0530

    update

--- script.js
+++ script.js
@@ -138,3 +138,41 @@ document.addEventListener("DOMContentLoaded", function () {
   yrSpan.textContent = currentYr;
 
 })
+
+
+
+
+const counters = document.querySelectorAll('.counter');
+let hasAnimated = false;
+
+const animateCounters = () => {
+  counters.forEach(counter => {
+    const updateCount = () => {
+      const target = +counter.getAttribute('data-target');
+      const count = +counter.innerText;
+      const increment = target / 100;
+
+      if (count < target) {
+        counter.innerText = Math.ceil(count + increment);
+        setTimeout(updateCount, 20);
+      } else {
+        counter.innerText = target;
+      }
+    };
+    updateCount();
+  });
+};
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting && !hasAnimated) {
+      animateCounters();
+      hasAnimated = true;
+    }
+  });
+}, {
+  threshold: 0.3,
+});
+
+observer.observe(document.getElementById('counters'));
+
